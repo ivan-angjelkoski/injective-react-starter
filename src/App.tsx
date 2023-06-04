447c139cfa03c15791069ba3f27cbd6f8ca08683
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2023-06-04 04:11:13 +0200

    feat: react injective app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import InjectiveWelcome from "./InjectiveWelcome";
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <InjectiveWelcome />
+    </div>
+  );
+};
+
+export default App;
